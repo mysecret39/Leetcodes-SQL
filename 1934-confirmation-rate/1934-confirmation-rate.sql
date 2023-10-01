35cd8a817d57bc0212030d15151bc5aeca4fb2c5
Jonathan <88777199+mysecret39@users.noreply.github.com>
2023-10-01 15:25:31 +0700

    Time: 1141 ms (45.37%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,12 +1,12 @@
 /* Write your T-SQL query statement below */
 
-SELECT c.user_id, COALESCE(CAST((CAST(confirmation AS DECIMAL(10,2)) /CAST(timeout AS DECIMAL(10,2))) AS DECIMAL(10,2)),0) AS confirmation_rate FROM
-(SELECT user_id FROM signups) AS c
-LEFT JOIN
-(SELECT user_id, COUNT(*) AS timeout FROM confirmations
-GROUP BY user_id) AS a ON c.user_id = a.user_id
-LEFT JOIN 
-(SELECT user_id, COUNT(*) AS confirmation FROM confirmations
-WHERE action = 'confirmed'
-GROUP BY user_id
-) AS b ON a.user_id = b.user_id
+SELECT s.user_id
+	,ROUND(CAST(SUM(CASE 
+				WHEN action = 'confirmed'
+					THEN 1
+				ELSE 0
+				END) AS DECIMAL(10, 2)) / CAST(COUNT(*) AS DECIMAL(10, 2)), 2) AS confirmation_rate
+FROM Signups AS s
+LEFT JOIN Confirmations AS c
+	ON c.user_id = s.user_id
+GROUP BY s.user_id
